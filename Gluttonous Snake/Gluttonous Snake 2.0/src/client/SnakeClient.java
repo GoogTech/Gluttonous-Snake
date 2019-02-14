8eab3d03b057ad208bd41a86bc9df8f1c7755b58
HuangYuhui <Gentleman_0109@outlook.com>
2019-02-14 18:18:43 +0800

    Modify the project path of game.

---
+++ Gluttonous Snake/Gluttonous Snake 2.0/src/client/SnakeClient.java
@@ -0,0 +1,125 @@
+package client;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import constant.Constant;
+import core.Food;
+import core.MyFrame;
+import core.MySnack;
+import util.ImageUtil;
+import util.MusicPlayer;
+
+/**
+ * 
+ * @Project Snack 2.0
+ * @Package client
+ * @Description Load the JFrame and Control the game flow.
+ * @Author HuangYuhui
+ * @Date Feb 11, 2019-3:25:11 PM
+ * @version 2.0
+ */
+public class SnakeClient extends MyFrame
+{
+
+	public MySnack mySnack = new MySnack(100, 100);// x , y
+	public Food food = new Food();
+
+	Image background = ImageUtil.images.get("UI-background");
+	Image fail = ImageUtil.images.get("game-scene-01");
+
+	
+//	@Override
+//	public void loadFrame()
+//	{
+//		super.loadFrame();
+//		// Mouse clicked.
+//		this.addKeyListener(new KeyAdapter()
+//		{
+//			@Override
+//			public void keyPressed(KeyEvent e)
+//			{
+//				super.keyPressed(e);
+//				mySnack.keyPressed(e);
+//			}
+//		});
+//	}
+
+	/*
+	 * Listen for the event.
+	 * 
+	 * @see core.MyFrame#keyPressed(java.awt.event.KeyEvent)
+	 */
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		super.keyPressed(e);
+		mySnack.keyPressed(e);
+	}
+	
+
+	/*
+	 * Draw the game.
+	 * 
+	 * @see java.awt.Window#paint(java.awt.Graphics)
+	 */
+	@Override
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		g.drawImage(background, 0, 0, null); 
+
+		// Determine the state of the game.
+		if (mySnack.live)
+		{
+			mySnack.draw(g);
+			if (food.live)
+			{
+				food.draw(g);
+				food.eaten(mySnack);
+			} else
+			{
+				food = new Food();
+			}
+		} else
+		{
+			g.drawImage(fail, 0, 0, null);
+		}
+		drawScore(g);
+	}
+
+	/**
+	 * @Title Draw
+	 * @Description Draw the score.
+	 * @param Graphics g
+	 * @return void
+	 * @date Feb 11, 2019-3:39:35 PM
+	 *
+	 */
+	public void drawScore(Graphics g)
+	{
+		g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+		g.setColor(Color.MAGENTA);
+		g.drawString("SCORE : " + mySnack.score, 20, 40);
+	}
+
+	/**
+	 * @Title Main
+	 * @Description Test the project.
+	 * @return void
+	 * @date Feb 11, 2019-3:40:11 PM
+	 *
+	 */
+	public static void main(String[] args)
+	{
+		// Play the game.
+		new SnakeClient().loadFrame();
+
+		// Play the background music.
+		MusicPlayer.getMusicPlay(Constant.MUSIC_PRE + "Happy birthday.mp3");
+
+	}
+}
---
+++ Gluttonous Snake/Gluttonous Snake 2.0/src/core/MyFrame.java
@@ -0,0 +1,146 @@
+package core;
+
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import constant.Constant;
+
+/**
+ * 
+ * @Project Snack 2.0
+ * @Package score
+ * @Description Load the game and refresh it constantly.
+ * @Author HuangYuhui
+ * @Date Feb 11, 2019-10:20:42 AM
+ * @version 2.0
+ */
+public class MyFrame extends JPanel implements KeyListener
+{
+	JFrame jFrame = new JFrame();
+
+	/**
+	 * @Description SetIconImage
+	 * @Date Feb 11, 2019-6:22:15 PM
+	 */
+	public MyFrame()
+	{
+		jFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(MyFrame.class.getResource("/image/snack-logo.png")));
+	}
+
+	/*
+	 * Load the JFrame.
+	 */
+	public void loadFrame()
+	{
+		/*
+		 * Attention :Prevent the picture from flashing.
+		 */
+		this.setDoubleBuffered(true);
+		/*
+		 * Attention :Add JPanel into the JFrame.
+		 */
+		jFrame.add(this);
+		/*
+		 * Attention :Listen the event.
+		 */
+		jFrame.addKeyListener(this);
+
+		jFrame.setTitle("SNACK 2.1");
+		jFrame.setSize(Constant.GAME_WIDTH, Constant.GAME_HEIGHT);
+		jFrame.setLocationRelativeTo(null);// Center
+		// jFrame.setDefaultCloseOperation();
+		jFrame.addWindowListener(new WindowAdapter()// Close
+		{
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				super.windowClosing(e);
+				System.exit(0);
+			}
+		});
+		jFrame.setVisible(true);
+
+		/*
+		 * Thread-repaint constantly.
+		 */
+		new MyThread().start();
+	}
+
+	/*
+	 * Prevent the picture from flashing.
+	 */
+//	Image backImg = null;
+//	@Override
+//	public void update(Graphics g)
+//	{
+//		super.update(g);
+//		if (backImg == null)
+//		{
+//			backImg = createImage(Constant.GAME_WIDTH, Constant.GAME_HEIGHT);
+//		}
+//		Graphics back_g = backImg.getGraphics();
+//		Color color = back_g.getColor();
+//
+//		back_g.setColor(Color.BLACK);
+//		back_g.fillRect(0, 0, Constant.GAME_WIDTH, Constant.GAME_HEIGHT);
+//		back_g.setColor(color);
+//
+//		paint(back_g);
+//		g.drawImage(backImg, 0, 0, null);
+//	}
+
+	/**
+	 * @Project Snack 2.0
+	 * @Package score
+	 * @Description repaint constantly.
+	 * @Author HuangYuhui
+	 * @Date Feb 11, 2019-10:43:18 AM
+	 * @version 2.0
+	 */
+	class MyThread extends Thread
+	{
+		@Override
+		public void run()
+		{
+			super.run();
+			while (true)
+			{
+				repaint();// Repaints this component.
+				try
+				{
+					sleep(30);// 30ms/repaint once.
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
